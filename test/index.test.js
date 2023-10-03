4876a493979ab7c21349c053617827c701dac8a2
Lê Huy Giang <lehuygiang28@gmail.com>
2023-10-04 01:29:40 +0700

    add test case

--- test/index.test.js
+++ test/index.test.js
@@ -6,17 +6,37 @@ describe('generateRegexQuery', () => {
         const options = {
             keyword: 'Hà Nội',
         };
-        const expectedRegex = new RegExp(`[h][à][ ][n][ộ][${caseI}]`, 'i');
+        const expectedRegex = new RegExp(`[H][à][ ][N][ộ][${caseI}]`, 'i');
         expect(generateRegexQuery(options)).toEqual(expectedRegex);
     });
 
     it('should generate a regular expression that matches the keyword with or without diacritical marks and with different variations of the same letter', () => {
         const options = {
             keyword: 'Hà Nội',
-            caseSensitive: false,
-            ignorePunctuation: true,
+            sensitive: true,
+            ignoreAccentedVietnamese: true,
         };
-        const expectedRegex = new RegExp(`[h][${caseA}][ ][n][${caseO}][${caseI}]`, 'i');
+        const expectedRegex = new RegExp(`[H][${caseA}][ ][N][${caseO}][${caseI}]`);
+        expect(generateRegexQuery(options)).toEqual(expectedRegex);
+    });
+
+    it('should generate a regular expression that have sensitive case', () => {
+        const options = {
+            keyword: 'Hà Nội',
+            sensitive: false,
+            ignoreAccentedVietnamese: true,
+        };
+        const expectedRegex = new RegExp(`[H][${caseA}][ ][N][${caseO}][${caseI}]`, 'i');
+        expect(generateRegexQuery(options)).toEqual(expectedRegex);
+    });
+
+    it('should generate a regular expression that not ignore accented', () => {
+        const options = {
+            keyword: 'Hà Nội',
+            sensitive: false,
+            ignoreAccentedVietnamese: false,
+        };
+        const expectedRegex = new RegExp(`[H][à][ ][N][ộ][${caseI}]`, 'i');
         expect(generateRegexQuery(options)).toEqual(expectedRegex);
     });
 
