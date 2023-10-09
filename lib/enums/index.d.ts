efdfd0fadf025978586b3be2174428d8e9f79b24
Lê Huy Giang <lehuygiang28@gmail.com>
2023-10-09 11:29:01 +0700

    ignore lib folder

--- lib/enums/index.d.ts
+++
@@ -1,6 +0,0 @@
-export declare enum OutputCaseOptions {
-    lowercase = "lowercase",
-    uppercase = "uppercase",
-    both = "both",
-    sameInput = "sameInput"
-}
--- lib/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { OutputCaseOptions } from '../enums';
-export type TGenerateSearchQuery = {
-    keyword: string;
-    sensitive?: boolean;
-    ignoreAccentedVietnamese?: boolean;
-    outputCaseOptions?: OutputCaseOptions | string;
-};
