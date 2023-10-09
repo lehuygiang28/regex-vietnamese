75fe29ebc9880e1bad1798da6f23c841e848c1be
Lê Huy Giang <lehuygiang28@gmail.com>
2023-10-09 11:38:49 +0700

    update(test): change to test in dev mode with typescripts

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,9 +1,9 @@
-const { generateRegexQuery } = require('../lib');
-const { caseA, caseE, caseI, caseO, caseU, caseY, caseD } = require('../lib/constants');
+import { generateRegexQuery, TGenerateSearchQuery } from '..';
+import { caseA, caseE, caseI, caseO, caseU, caseY, caseD } from '../constants';
 
 describe('generateRegexQuery', () => {
     it('should generate a regular expression that matches the keyword with or without diacritical marks and with different variations of the same letter', () => {
-        const options = {
+        const options: TGenerateSearchQuery = {
             keyword: 'Hà Nội',
         };
         const expectedRegex = new RegExp(`[H][à][ ][N][ộ][${caseI}]`, 'i');
@@ -11,7 +11,7 @@ describe('generateRegexQuery', () => {
     });
 
     it('should generate a regular expression that matches the keyword with or without diacritical marks and with different variations of the same letter', () => {
-        const options = {
+        const options: TGenerateSearchQuery = {
             keyword: 'Hà Nội',
             sensitive: true,
             ignoreAccentedVietnamese: true,
@@ -21,7 +21,7 @@ describe('generateRegexQuery', () => {
     });
 
     it('should generate a regular expression that have sensitive case', () => {
-        const options = {
+        const options: TGenerateSearchQuery = {
             keyword: 'Hà Nội',
             sensitive: false,
             ignoreAccentedVietnamese: true,
@@ -31,7 +31,7 @@ describe('generateRegexQuery', () => {
     });
 
     it('should generate a regular expression that not ignore accented', () => {
-        const options = {
+        const options: TGenerateSearchQuery = {
             keyword: 'Hà Nội',
             sensitive: false,
             ignoreAccentedVietnamese: false,
@@ -41,16 +41,16 @@ describe('generateRegexQuery', () => {
     });
 
     it('should throw an error if the keyword parameter is undefined', () => {
-        const options = {
+        const options: TGenerateSearchQuery = {
             keyword: undefined,
-        };
+        } as any;
         expect(() => generateRegexQuery(options)).toThrow('Keyword parameter is required');
     });
 
     it('should throw an error if the keyword parameter is not a string', () => {
-        const options = {
+        const options: TGenerateSearchQuery = {
             keyword: 123,
-        };
+        } as any;
         expect(() => generateRegexQuery(options)).toThrow('Keyword parameter must be a string');
     });
 });
